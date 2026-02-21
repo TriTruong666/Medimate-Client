0d7f0da44d79c12906bf111789cb8880af765542
TriTruong666 <tritruonghoang3@gmail.com>
2026-02-22 01:29:19 +0700

    feat: fix error dependencies, add test loading animation

--- src/routes/publicRoute.tsx
+++ src/routes/publicRoute.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import LoginLayout from "../layouts/LoginLayout";
 import LoginPage from "../pages/LoginPage";
-import Welcome from "../components/Welcome";
 import ForgetPassPage from "../pages/ForgetPassPage";
+import TestPage from "../pages/TestPage";
 
 export default function PublicRoute() {
   return (
@@ -10,6 +10,7 @@ export default function PublicRoute() {
       <Route element={<LoginLayout />}>
         <Route path="/" element={<LoginPage />} />
         <Route path="/forget-password" element={<ForgetPassPage />} />
+        <Route path="/test" element={<TestPage />} />
       </Route>
     </Routes>
   );
